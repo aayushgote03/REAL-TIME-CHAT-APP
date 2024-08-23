197fa953ba3c5e145b4cb7933c6b3a82d06dc703
aayushgote03 <aayushgote03@gmail.com>
2024-08-23 22:56:47 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://real-chat-bu5l.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
